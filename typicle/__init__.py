9b6c90146bc00c2802ada95e56d9926964064324
Jacan Chaplais <jlc1n20@soton.ac.uk>
2021-12-16 18:53:56 +0000

    made internal struc types immutable; added coverage for pdg and final

--- typicle/__init__.py
+++ typicle/__init__.py
@@ -13,23 +13,27 @@ def parse_yaml(path: str):
 def struc_type(type_dict: dict) -> list:
     return list(type_dict.items())
 
+_immutable_struc = lambda x: tuple(struc_type(x))
+
 class Types:
     def __init__(self, config_file: str=str(THIS_DIR / 'types.yml')):
         data = parse_yaml(config_file)
         for key, val in data['TYPE'].items():
             setattr(self, key, val)
-        self.__pmu = data['PMU_DTYPE']
-        self.__edge = data['EDGE_DTYPE']
-        self.__color = data['COLOR_DTYPE']
+        for key, val in data['PHYSICS_TYPE'].items():
+            setattr(self, key, val)
+        self.__pmu = _immutable_struc(data['PMU_DTYPE'])
+        self.__edge = _immutable_struc(data['EDGE_DTYPE'])
+        self.__color = _immutable_struc(data['COLOR_DTYPE'])
 
     @property
     def pmu(self):
-        return struc_type(self.__pmu)
+        return list(self.__pmu)
 
     @property
     def edge(self):
-        return struc_type(self.__edge)
+        return list(self.__edge)
 
     @property
     def color(self):
-        return struc_type(self.__color)
+        return list(self.__color)
